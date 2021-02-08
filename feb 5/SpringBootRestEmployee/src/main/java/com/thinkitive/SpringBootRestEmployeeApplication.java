0344cc18ad2bd8456d6232ad694ea74dd4dd6860
Reshikesh Dhanrale <77092447+Reshikesh1998@users.noreply.github.com>
2021-02-08 10:09:20 +0530

    feb 5 codes

---
+++ feb 5/SpringBootRestEmployee/src/main/java/com/thinkitive/SpringBootRestEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.thinkitive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestEmployeeApplication.class, args);
+	}
+
+}
